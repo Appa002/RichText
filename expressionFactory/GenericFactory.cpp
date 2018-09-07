42476e1f27423ed215b895e140071a9e41c364c4
batburger <a_modenbach@outlook.com>
2018-09-07 14:53:09 +0200

    Moved widget creation to GenericFactorWidget class

--- expressionFactory/GenericFactory.cpp
+++ expressionFactory/GenericFactory.cpp
@@ -1,14 +1,6 @@
 #include "GenericFactory.h"
-
-#include <QLabel>
-#include <QDebug>
-#include <QFrame>
+#include "GenericFactoryWidget.h"
 #include <QLayout>
-#include <QMainWindow>
-#include <QApplication>
-#include <QWindow>
-#include <QResizeEvent>
-
 
 GenericFactory::GenericFactory() : IExpressionFactory ()
 {
@@ -27,30 +19,12 @@ void GenericFactory::setup(QWidget *attachedTo)
 
 void GenericFactory::createWidgets()
 {
-
-    QPalette pallet;
-    pallet.setBrush(QPalette::Window, QBrush(QColor(0x39, 0x39, 0x39)));
-    pallet.setBrush(QPalette::WindowText, QBrush(QColor(0xff, 0xff, 0xff)));
-
-    QFrame* frame = new QFrame();
-    frame->setAutoFillBackground(true);
-    frame->setObjectName("expressionFactoryFrame");
-    frame->setPalette(pallet);
-    frame->setLayout(new QVBoxLayout());
-
-    QWidget* mainWindow = QApplication::topLevelWidgets()[0];
-    QCoreApplication::postEvent(mainWindow, new QResizeEvent(QSize(mainWindow->size().width(), mainWindow->size().height()),
-                                                             QSize(mainWindow->size().width(), mainWindow->size().height() + 1)));
-
-    QLabel* label = new QLabel;
-    label->setPalette(pallet);
-    label->setText("Generic");
-    frame->layout()->addWidget(label);
+    GenericFactoryWidget* widget = new GenericFactoryWidget();
 
     if(this->attachedTo->layout())
-        this->attachedTo->layout()->addWidget(frame);
+        this->attachedTo->layout()->addWidget(widget);
     else
-        frame->setParent(this->attachedTo);
+        widget->setParent(this->attachedTo);
 
-    frame->show();
+    widget->show();
 }
